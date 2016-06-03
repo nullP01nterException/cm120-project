6a139486a8df19689839cead7ca72f427a24ff0b
Richard Harker <rharker7@gmail.com>
2016-06-03 01:47:41 -0700

    Updated win screen to include reef expansion

---
+++ resultScreen.js
@@ -0,0 +1,38 @@
+function drawWin(){
+	drawWave();
+
+	//draw tinted coral
+	//tint initialize
+	//tintedCoralUpdate();
+	updateCoral();
+	//tintedCoralDraw();
+	drawCoral();
+	context.font = "32px Verdana";
+	context.fillStyle = 'white';
+	context.fillText("You Win!", canvas.width/2-50, canvas.height/2);
+	context.font = "25px Verdana";
+	context.fillText("Click to return to main menu", canvas.width/2-175, canvas.height/2+50);
+
+	if(mouseClicked){
+		state = 0;
+		win = null;
+		alive = true;
+		resetGame();
+	}
+}
+
+function drawLose(){
+	background();
+	context.font = "32px Verdana";
+	context.fillStyle = 'white';
+	context.fillText("You Lose...", canvas.width/2-50, canvas.height/2);
+	context.font = "25px Verdana";
+	//context.fillText("Click to return to main menu", canvas.width/2-175, canvas.height/2+50);
+
+	if(mouseClicked){
+		state = 0;
+		win = null;
+		alive = true;
+		resetGame();
+	}
+}
